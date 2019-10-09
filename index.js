2e1f5ed9bf976071ffbe54ce4ac7838c2e984ca3
FIAP <FIAP>
2019-10-08 22:37:11 -0300

    Primeira Rota

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.get('/usuarios', (req, res, next) => {
     })
 })
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Escutando a porta: ${port}`) // Pegar variavel de ambiente
+    console.log('Escutando a porta:' + port) // Pegar variavel de ambiente
 })
