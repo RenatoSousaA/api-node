0d2f892bb0087cc2e2e1c6a67a0a1c8a5fafc233
FIAP <FIAP>
2019-10-10 22:30:48 -0300

    Integracao com o Firebase

---
+++ config/firebase.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyD2FzhKdaxhla7H8U4Wh_G8C3ux0Zv_-4g",
+    authDomain: "renato-node-api.firebaseapp.com",
+    databaseURL: "https://renato-node-api.firebaseio.com",
+    projectId: "renato-node-api",
+    storageBucket: "renato-node-api.appspot.com",
+    messagingSenderId: "800431847753",
+    appId: "1:800431847753:web:05bc77ff5b33b6970d8298",
+    measurementId: "G-YGF8Q6E0PQ"
+}
--- index.js
+++ index.js
@@ -1,20 +1,49 @@
 // console.log(`Meu ambiente: ${process.env.NODE_ENV}`) // Pegar variavel de ambiente
 
-const express = require('express')
+// Express
+const express = require('express');
+
+// Firebase
+const firebase = require('firebase');
+const firebaseConfig = require('./config/firebase');
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+// APP
 const app = express();
 
-app.get('/usuarios', (req, res, next) => {
-    //console.log(req.query.id);
-    //res.sendStatus(200);
-    console.log('ENTREI NA ROTA USUARIOS')
-    res.json({
-        success: true
-    })
+app.get('/users/:id', (req, res, next) => {
+    const id = req.params.id;
+    db.collection('users').doc(id).get()
+        .then(user => {
+            if (!user.exists) {
+                return res.sendStatus(204)
+            }
+
+            res.json(user.data())
+        })
+        .catch(err => {
+            res.sendStatus(500);
+            console.log(err);
+        })
+})
+
+app.get('/users', (req, res, next) => {
+    db.collection('users').get()
+        .then(users => {
+            let allUser = [];
+            users.forEach(user => {
+                allUser.push(user.data());
+            });
+            res.json(allUser);
+        })
+        .catch(err => {
+            res.sendStatus(500);
+            console.log(err);
+        })
 })
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Escutando a porta: ${port}`) // Pegar variavel de ambiente
-    // console.log('Escutando a porta:' + port) // Pegar variavel de ambiente
 })
